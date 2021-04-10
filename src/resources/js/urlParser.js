2879b490de879865635a8236b48b217a2545ca02
カジタ ヒデマサ <hidemasa_kajita@respawn.jp>
2021-04-10 17:52:11 +0900

    pugを全体的に修正

--- src/resources/js/urlParser.js
+++ src/resources/js/urlParser.js
@@ -8,5 +8,13 @@ const isError = params.get('is_error')
 
 if (isError) {
   const elem = document.getElementById('error')
-  elem.innerHTML = '各項目の値を確認してください。'
+  elem.classList.add('alert')
+  elem.classList.add('alert-danger')
+
+  if (url.pathname === '/login') {
+    elem.innerHTML = 'Failed to login.'
+    return 
+  }
+
+  return 'Failed to register.'
 }
