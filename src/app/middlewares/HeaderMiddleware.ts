c71c0bdf17cd768bfb784a2169daa0468d8892f4
カジタ ヒデマサ <hidemasa_kajita@respawn.jp>
2021-04-11 11:08:24 +0900

    レスポンスヘッダーを設定するミドルウェア修正

--- src/app/middlewares/HeaderMiddleware.ts
+++ src/app/middlewares/HeaderMiddleware.ts
@@ -1,12 +1,12 @@
 import { Middleware, ExpressMiddlewareInterface } from "routing-controllers"
 
 /**
- * ルーティングミドルウェア
+ * レスポンスヘッダーを設定するミドルウェア
  */
 @Middleware({ type: 'after' })
 export class HeaderMiddleware implements ExpressMiddlewareInterface {
   use(req: any, res: any, next: (err?: any) => any): void {
-    res.setHeader('Cache-Control: no-store')
+    res.setHeader('Cache-Control', 'no-store')
 
     next()
   }
