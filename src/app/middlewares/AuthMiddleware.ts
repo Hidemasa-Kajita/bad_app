c8a9c40937848e3e1ffe81f1385bc2a4689a6c7c
カジタ ヒデマサ <hidemasa_kajita@respawn.jp>
2021-04-11 11:26:36 +0900

    リダイレクトを302に変更

--- src/app/middlewares/AuthMiddleware.ts
+++ src/app/middlewares/AuthMiddleware.ts
@@ -6,7 +6,7 @@ import { ExpressMiddlewareInterface } from 'routing-controllers'
 export class AuthMiddleware implements ExpressMiddlewareInterface {
   use(req: any, res: any, next: (err?: any) => any): void {
     if (req.session.user === undefined) {
-      res.redirect(301, '/login')
+      res.redirect(302, '/login')
       return res
     }
 
